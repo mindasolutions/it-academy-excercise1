af6fdd0641d291ec330dd20794883b5a95a84c28
Dawid Minda <dawid@minda.pl>
2019-02-19 04:33:59 +0100

    Delete generate random values, had multiple errors.

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,28 +3,15 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class MainClass {
     public static void main(String[] args) {
-        //initialize default value
-        int min = 0;
-        int max = 50;
-
-        int a = ThreadLocalRandom.current().nextInt(min, max + 1);
-        int b = ThreadLocalRandom.current().nextInt(min, max + 1);
-
         // get 'a' variable from user
-        System.out.print("Type a [" + a + "]: ");
+        System.out.print("Type a: ");
         Scanner takeA  = new Scanner(System.in);
-        //press enter to continue with default value
-        if(!takeA.hasNextLine()){
-            a = takeA.nextInt();
-        }
+        int a = takeA.nextInt();
 
         // get 'b' variable from user
-        System.out.print("Type b [" + b + "]: ");
+        System.out.print("Type b: ");
         Scanner takeB = new Scanner(System.in);
-        //press enter to continue with default value
-        if(!takeB.hasNextLine()){
-            b = takeB.nextInt();
-        }
+        int b = takeB.nextInt();
 
         //initialize class with variable
         CalculatorClass result = new CalculatorClass(a, b);
